e5a1d8bfdebd04dc1d97d19d1a40de16a2275caa
mpaynezen <73001527+mpaynezen@users.noreply.github.com>
2020-12-29 16:25:22 -0600

    Add files via upload

---
+++ dpaupdate.ps1
@@ -0,0 +1,3 @@
+﻿Stop-Service -Name DCUWindowsService
+(Get-Content C:\ProgramData\Verint\DPA\Data\PcMonConfig.ini).replace('five9verint', 'verint02') | Set-Content C:\ProgramData\Verint\DPA\Data\PcMonConfig.ini
+Start-service -Name DCUWindowsService
